a1a101a349086e9ec3fa249ed8295a6091d6a2c3
Zach.Golkhou <golkhou@gmail.com>
2020-08-08 20:42:40 -0700

    Update visualizing_twitter_data.py

--- visualizing_twitter_data.py
+++ visualizing_twitter_data.py
@@ -139,7 +139,7 @@ if __name__ == '__main__':
     
     time_favs = pd.Series(data=df['likes'].values, index=df['date'])
     time_favs.plot(figsize=(16, 4), color='r')
-    #plt.show()
+    plt.show()
 
     #time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
     #time_retweets.plot(figsize=(16, 4), color='r')
